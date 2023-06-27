15ef86d0dde83e3da15b0c93316af414a04d5b17
WanjaMaryanne <maryannewanja4@gmail.com>
2023-06-27 15:18:25 +0300

    typo fix

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -15,7 +15,7 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 
 	printf("Last digit of %d is ", n);
-	
+
 	last_number = n % 10;
 
 	if (last_number	> 5)
@@ -26,7 +26,9 @@ int main(void)
 	{
 		printf("%d and is less than 6 and not 0\n", last_number);
 	}
-	
+	{
+		printf("%d and is 0\n", last_number);
+	}
 	return (0);
 }
 
