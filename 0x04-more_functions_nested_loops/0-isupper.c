c8745efb8722231ce4fa58184ed4ab91c462368a
WanjaMaryanne <maryannewanja4@gmail.com>
2023-07-05 00:04:02 +0300

    a function that checks for a digit (0 through 9)

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,15 +2,14 @@
 
 /**
  * _isupper - check if the letter is  upper
- * 0c : char to check
- *
+ *@0c : char to check
  * Return: 0 or 1
  */
-
 int _isupper(int c)
 {
-	if (c >= 'A' && c <= '2')
-	return (1);
-	else
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
 	return (0);
 }
