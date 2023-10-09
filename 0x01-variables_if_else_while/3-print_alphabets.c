b72300fbe89ba42f21fef15f7479dc68f4e6e8f9
WanjaMaryanne <maryannewanja4@gmail.com>
2023-10-09 07:25:00 -0700

    betty

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -7,20 +7,19 @@
  *
  */
 
-int main (void)
+int main(void)
 {
 	char letter;
 
-	for(letter = 'a'; letter <= 'z'; letter++)
+	for (letter = 'a'; letter <= 'z'; letter++)
 	{
 		putchar(letter);
 	}
-	for(letter ='A'; letter <= 'Z'; letter++)
+	for (letter = 'A'; letter <= 'Z'; letter++)
 	{
 		putchar(letter);
 	}
 	putchar('\n');
-	
-	return(0);
-	
+
+	return (0);
 }
