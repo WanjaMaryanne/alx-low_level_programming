c27cde21cd2563b5a16340cc21f8f623c4cb2dd4
WanjaMaryanne <maryannewanja4@gmail.com>
2023-10-25 06:40:04 -0700

    code fix

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
 * add -  adds two integers and returns the result.
-*@x: number  being added.
-*@y: number being added.
+*@x:first number  being added.
+*@y:second number being added.
 * Return: Always 0.
 */
 int add(int x, int y)
