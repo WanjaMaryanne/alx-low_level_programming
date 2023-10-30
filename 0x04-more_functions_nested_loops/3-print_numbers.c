ef4691edac799429ccc1f963005514bd4bcedb75
WanjaMaryanne <maryannewanja4@gmail.com>
2023-10-30 13:52:05 +0300

    code fix

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,14 +1,14 @@
 #include "main.h"
 /**
 * print_numbers - prints the numbers, from 0 to 9
-*
+*@c: number to be printed
 * Return: Always 0 (success)
 */
 void print_numbers(void)
 {
 	int c;
 
-	for (c >= 48; c <= 57; c++)
+	for (c = 48; c <= 57; c++)
 	{
 		_putchar(c);
 	}
