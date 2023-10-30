592ea3f56a30c87bc663ba967c286305ccc7aacd
WanjaMaryanne <maryannewanja4@gmail.com>
2023-10-30 15:27:50 +0300

    6-print_line.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -5,15 +5,15 @@
 */
 void print_most_numbers(void)
 {
-	int c;
+        int c;
 
-	c = 0;
-	while (c < 10)
-	{
-		if (c != 2 && c != 4)
-			_putchar(c + '0');
+        c = 0;
+        while (c < 10)
+        {
+                if (c != 2 && c != 4)
+                        _putchar(c + '0');
 
-		c++;
-	}
-	_putchar('\n');
+                c++;
+        }
+        _putchar('\n');
 }
