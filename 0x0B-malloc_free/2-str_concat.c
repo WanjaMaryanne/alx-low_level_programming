6af698b4564983055fae2a168e1eee355782bd3d
WanjaMaryanne <maryannewanja4@gmail.com>
2023-11-01 12:48:01 +0300

    code fix 2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -21,7 +21,7 @@ char *str_concat(char *s1, char *s2)
 	for (i = 0; s1[i] || s2[i]; i++)
 		len++;
 
-	ptr = (char *)malloc(sizeof(char) * (len + 1));
+	ptr = (char *)malloc(sizeof(char) * (len));
 
 	if (ptr == 0)
 		return (NULL);
