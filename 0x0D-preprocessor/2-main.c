9c4c3e3b8f320c36b87607743d6cec76895429ae
WanjaMaryanne <maryannewanja4@gmail.com>
2023-11-06 14:36:06 +0300

    corrections for 2-main.c

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
- * main - program that prints the name of the file it was 
+ * main - program that prints the name of the file it was
  * complied from follwed by a new line
- *@FILE: file use for compilation
+ *
  * Return: 0
  */
 int main(void)
