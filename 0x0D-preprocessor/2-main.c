066ec1afb81ef6676fcfb8bf754e840baddaf7fc
WanjaMaryanne <maryannewanja4@gmail.com>
2023-11-06 14:27:55 +0300

    2-main.c

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - program that prints the name of the file it was 
+ * complied from follwed by a new line
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	printf("%s\n", _FILE_);
+
+	return (0);
+}
