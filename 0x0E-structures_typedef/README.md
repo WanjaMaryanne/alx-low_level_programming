a97784e4d9ee61b77f00c0af7d8def647a9644ad
WanjaMaryanne <maryannewanja4@gmail.com>
2023-11-06 20:18:43 +0300

    dog.h

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,2 @@
+Welcome to the Data Structures and Typedef Project! This project provides a collection of data structures and typedefs to facilitate data organization and manipulation in your programming projects. Whether you're working on a small application or a large software system, these data structures and typedefs can help streamline your code and improve maintainability.
+
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,25 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - describes dog
+ * @name:name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+int _putchar(char);
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+
+typedef struct dog dog_t;
+
+dog_t *new_dog(char *name, float age, char *owner);
+
+void free_dog(dog_t *d);
+#endif
