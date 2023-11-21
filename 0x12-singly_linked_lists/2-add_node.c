7a42aa87f52f10fc822dc5988de99a4c2ee12cf1
WanjaMaryanne <maryannewanja4@gmail.com>
2023-11-21 13:45:55 +0300

    correction for 2-add_node.c

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 #include <string.h>
 /**
- * add_node - adds a new node at the beginning
+ * add_node - adds a new node at the beginning of the list_t
  * @head: pointer to the head of the list_t
  * @str: string to be duplicated
  *
@@ -12,7 +12,7 @@ list_t *add_node(list_t **head, const char *str)
 	list_t *new;
 	unsigned int i;
 
-	new = malloc(sizeof(list_t *));
+	new = malloc(sizeof(list_t));
 
 	if (new == NULL)
 		return (NULL);
