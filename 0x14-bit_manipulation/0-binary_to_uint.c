d27c1b53071392b09051ec141ce00fe47990e1af
WanjaMaryanne <maryannewanja4@gmail.com>
2023-11-24 16:46:47 +0300

    0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -14,7 +14,7 @@ unsigned int binary_to_uint(const char *b)
 	unsigned int index, decimal;
 	unsigned int i, j, k;
 
-	index = decimal = j = k = 0;
+	index = decimal = j = i = 0;
 
 	if (b == NULL)
 		return (0);
